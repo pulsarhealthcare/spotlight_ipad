03aa3ad1c17d73a2f2c5a7e367bcd7ddacd82f96
unknown <C_FXA@APSHQW652.APSGROUP.LOCAL>
2014-07-31 15:31:28 +0100

    init commit gary's changes

---
+++ presentation_macilax_07/slide_macilax_00/index.php
@@ -0,0 +1,24 @@
+<?php $root = $_SERVER["DOCUMENT_ROOT"];?>
+<?php require $root.'/global/html/header.html';?>
+<?php require $root.'/global/html/menu.html';?>
+
+<section style="background:url(img/background.png);background-size:100% 100%;" id="slide_container">
+                            
+<h2>Prescribing Macilax<sup>®</sup> by brand name is the least expensive way of prescribing macrogol<br />13.8 g single-dose sachets and can offer savings of up to £3.12 per prescription dispensed,<br /></h2>
+<h2>compared to prescribing Movicol<sup>®</sup> or prescribing generically<sup>*†1</sup></h2>
+            
+
+
+
+
+            <div id="notes">
+                <p>*Calculation based on macrogol 13.8g single dose sachets (50 pack size)</p>
+                
+                <p>†Amongst products which are available and have a secure supply chain</p>
+                
+               
+            </div>
+               <p class="side_note">Date of preparation: July 2014  UK/SPL/14/0017f</p>
+</section>
+        
+<?php require $root.'/global/html/footer.html';?>
