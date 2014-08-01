479bb24796193bd9a35f8630bdb7d0c20e7ecf3d
unknown <C_FXA@APSHQW652.APSGROUP.LOCAL>
2014-08-01 16:51:40 +0100

    added more slides

--- 01_presentation_what_is_spotlight/08_slide_reimbursement_scotland/index.php
+++ 01_presentation_what_is_spotlight/08_slide_reimbursement_scotland/index.php
@@ -3,7 +3,7 @@
 <?php require $root.'/global/html/menu.html';?>
 
 <section style="background:url(img/background.png);background-size:100% 100%;" id="slide_container">
-      <h1>Reimbursement - an example - England and Wales</h1>
+      <h1>Reimbursement - an example - Scotland</h1>
       <img id="main" width="508" src="img/main_0.png" />
       <img id="main_1" width="250" src="img/main_1.png" />
       <img id="main_2" width="250" src="img/main_2.png" />
