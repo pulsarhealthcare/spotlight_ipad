340eb6d0e3ccd53e89b6ff57e909be20d58233e4
unknown <C_FXA@APSHQW652.APSGROUP.LOCAL>
2014-08-04 14:17:13 +0100

    Added Potential Saving Slides

---
+++ 02_presentation_potential_savings/02_slide_potential_savings - Copy/index.php
@@ -0,0 +1,24 @@
+<?php $root = $_SERVER["DOCUMENT_ROOT"];?>
+<?php require $root.'/global/html/header.html';?>
+<?php require $root.'/global/html/menu.html';?>
+
+<section style="background:url(img/background.png);background-size:100% 100%;" id="slide_container">
+    <h1>Cost savings per prescription</h1>
+
+    <h2>Examples of Spotlight brand cost savings per prescription when compared to the originator brands</h2>
+    <img id="main" width="805" src="img/main.png">  
+    
+    <div id="notes">
+        <p>*Where the originator brand is no longer available for prescription the replacement brand has been used<p>
+
+        <p>**Gaviscon Advance is taken at half the dose of Gaviscon/Peptac, therefore savings are calculated against twice the volume of Peptac</p>
+        
+	</div>
+                            
+
+    <p class="side_note">Date of preparation: July 2014 UK/SPL/14/0017</p>
+
+
+</section>
+        
+<?php require $root.'/global/html/footer.html';?>
