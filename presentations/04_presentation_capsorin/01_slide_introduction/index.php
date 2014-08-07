91d14985ec5e6933b8aa186345b5584bc40424b9
garrypulsar <garrybain@apshqmac158.apsgroup.local>
2014-08-07 16:57:40 +0100

    End of day dump, started on Capsorin

---
+++ presentations/04_presentation_capsorin/01_slide_introduction/index.php
@@ -0,0 +1,61 @@
+<?php $root = $_SERVER["DOCUMENT_ROOT"];?>
+<?php require $root.'/global/html/header.html';?>
+<?php require $root.'/global/html/menu.html';?>
+
+<section style="background:url(img/background.png);background-size:100% 100%;" id="slide_container">
+    <h1>Introduction</h1>             
+
+
+<h2>
+
+Capsorin<sup>®</sup>
+costs approximately 33% less than
+Neoral<sup>®</sup>
+(ciclosporin 100 mg), which could deliver cost savings
+of up to £23.68 (100 mg, 30 pack size) per
+prescription. At a Primary Care Organisation (PCO)
+level, this translates into a potential average annual
+saving of <span>over £13,564<sup>1</sup></span>
+
+</h2>
+
+
+<h3>All products that contain ciclosporin should be
+                                prescribed by brand name to minimise the risk of
+                                inadvertent switching between brands, and to reflect
+                                advice in the <span>British National Formulary<sup>2</sup></span></h3>
+
+
+
+<h3>Brands of ciclosporin are not considered
+                                interchangeable without careful therapeutic monitoring.
+                                If switching a patient stabilised to one brand of
+                                ciclosporin is unavoidable, the patient should be
+                                closely monitored for side effects, ciclosporin drug
+                                blood concentrations, serum <span>creatinine levels, blood
+                                pressure, and transplant function<sup>2</sup></span></h3>
+
+
+
+<h3>Transfer from one ciclosporin product to another does not <span>require dose adjustment in the first
+                                instance<sup>2</sup></span></h3>
+
+
+    
+    <div id="notes">
+        <p>An average PCO represents a population of approximately
+                                272,000 people. Please note: the term Primary Care
+                                Organisation refers to Clinical Commissioning Groups
+                                (England), NHS Boards (Scotland), Local Health Boards
+                                (Wales) and Health and Social Care Trusts (Northern
+                                Ireland)</p>             
+
+        
+   
+    </div>
+
+       <p class="side_note">Date of preparation: July 2014  UK/SPL/14/0017m</p>
+</section>
+
+
+<?php require $root.'/global/html/footer.html';?>
---
+++ presentations/04_presentation_capsorin/02_slide_potential_cost_savings/index.php
@@ -0,0 +1,46 @@
+<?php $root = $_SERVER["DOCUMENT_ROOT"];?>
+<?php require $root.'/global/html/header.html';?>
+<?php require $root.'/global/html/menu.html';?>
+
+<section style="background:url(img/background.png);background-size:100% 100%;" id="slide_container">
+
+    <h1>Potential Spotlight cost savings<sup>1</sup></h1>             
+
+
+<h2>
+Capsorin<sup>®</sup>
+costs approximately 33% less than
+Neoral<sup>>®</sup>
+(ciclosporin 100 mg), which could deliver cost savings
+of up to £23.68 (100 mg, 30 pack size) per
+prescription. At a Primary Care Organisation (PCO)
+level, Capsorin offers potential average annual savings
+of over £13,564
+</h2>
+
+    <img class="fullscreen" id="main" src="img/main.png"/>
+
+    <div id="notes">
+        <p>*Note: due to strict guidance that transplant patients
+                                should not be switched between brands of ciclosporin
+                                without careful therapeutic monitoring, all
+                                calculations for the size of the ciclosporin market
+                                (volume and value) and the associated savings that
+                                Capsorin can deliver are only associated with
+                                non-transplant indications</p>
+
+
+     
+     <p>An average PCO represents a population of approximately
+                                272,000 people. Please note: the term Primary Care
+                                Organisation refers to Clinical Commissioning Groups
+                                (England), NHS Boards (Scotland), Local Health Boards
+                                (Wales) and Health and Social Care Trusts (Northern
+                                Ireland)</p>
+
+
+    </div>
+       <p class="side_note">Date of preparation: June 2014 UK/SPL/14/0017e</p>
+</section>
+        
+<?php require $root.'/global/html/footer.html';?>      
