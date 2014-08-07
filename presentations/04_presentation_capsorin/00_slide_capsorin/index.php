70475b633813afd56272a84e1b1c62607cb0ffcd
garrypulsar <garrybain@apshqmac158.apsgroup.local>
2014-08-07 15:56:31 +0100

    New files for Casorin

---
+++ presentations/04_presentation_capsorin/00_slide_capsorin/index.php
@@ -0,0 +1,35 @@
+<?php $root = $_SERVER["DOCUMENT_ROOT"];?>
+<?php require $root.'/global/html/header.html';?>
+<?php require $root.'/global/html/menu.html';?>
+
+<section style="background:url(img/background.png);background-size:100% 100%;" id="slide_container">
+                            
+            
+<h2> Prescribing Prestylon<sup>®</sup> by brand name is the
+least expensive way of prescribing eicosapentaenoic 460
+mg acid/docasahexaenoic 380 mg acid capsules* and can
+offer savings of up to £12.71 per prescription compared</h2>
+<h2>acid/docasahexaenoic acid)<sup>†‡§1</sup></h2>
+
+<div id="notes">
+             
+  <p>*eicosapentaenoic acid/docasahexaenoic acid is also
+                                known as omega-3-acid ethyl esters 90</p>
+
+  <p>†Calculations based on Omacor<sup>®</sup> 1 g 100 capsules.
+                                Eicosapentaenoic acid/docasahexaenoic acid capsules are
+                                not available generically in NHS Scotland</p>
+
+<p>‡Amongst products which are available and have a secure
+                                supply chain</p>
+
+<p>§Indications may not be identical; refer to product
+                                SmPCs</p>                
+
+</div>
+
+               <p class="side_note">Date of preparation: July 2014  UK/SPL/14/0017m</p>
+	
+	</section>
+        
+<?php require $root.'/global/html/footer.html';?>
