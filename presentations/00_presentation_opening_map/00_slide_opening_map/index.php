919b02aa82b8c3e52e57acb70f500d3978fb7e3b
garrypulsar <garrybain@apshqmac158.apsgroup.local>
2014-08-08 16:11:00 +0100

    Amended slides

--- presentations/00_presentation_opening_map/00_slide_opening_map/index.php
+++ presentations/00_presentation_opening_map/00_slide_opening_map/index.php
@@ -2,7 +2,7 @@
 <?php require $root.'/global/html/header.html';?>
 <?php require $root.'/global/html/menu.html';?>
 
-<section style="background:url(img/background.png);background-size:100% 100%;" id="slide_container">
+<section id="slide_container">
 
     <img id="main" width="653" src="img/map.png"/>
 </section>
--- presentations/05_presentation_evacal/03_slide_competitor_comparisons/js/script.js
+++ presentations/05_presentation_evacal/03_slide_competitor_comparisons/js/script.js
@@ -1,8 +1,7 @@
 var duration = 800;
 
-$('#slide_container img').delay((duration - 100) * 1).animate({opacity:1}, duration);
-$('#slide_container h2').delay((duration - 100) * 2).animate({opacity:1}, duration);
-$('#slide_container ul li').eq(0).delay((duration - 100) * 3).animate({opacity:1}, duration);
-$('#slide_container ul li').eq(1).delay((duration - 100) * 4).animate({opacity:1}, duration);
-$('#slide_container ul li').eq(2).delay((duration - 100) * 5).animate({opacity:1}, duration);
-$('.side_note').delay((duration - 100) * 6).animate({opacity:1}, duration);
+$('#slide_container h2').delay((duration - 100) * 1).animate({opacity:1}, duration);
+$('#slide_container img').delay((duration - 100) * 2).animate({opacity:1}, duration);
+$('#notes p').eq(0).delay((duration - 100) * 3).animate({opacity:1}, duration);
+$('#notes p').eq(1).delay((duration - 100) * 4).animate({opacity:1}, duration);
+$('.side_note').delay((duration - 100) * 5).animate({opacity:1}, duration);
