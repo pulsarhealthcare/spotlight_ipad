5983c448670eb9f1e5cabad0757cb043c03a32b8
garrypulsar <garrybain@apshqmac158.apsgroup.local>
2014-08-21 11:23:25 +0100

    Finished Matrifen

--- presentations/09_presentation_matrifen/03_slide_competitor_comparisons/index.php
+++ presentations/09_presentation_matrifen/03_slide_competitor_comparisons/index.php
@@ -9,8 +9,148 @@
 <h2>The table below highlights Prescribed Product NHS list price/reimbursement cost and the</h2>
 <h2>respective savings if Matrifen<sup>®</sup> is prescribed by brand<sup>1</sup></h2>
 
- <img class="fullscreen" src="img/competitor_comparisons.png" alt="" />                               
+<!-- <img class="fullscreen" src="img/competitor_comparisons.png" alt="" /> -->                               
     
+
+     <table>
+
+<thead>
+        <tr>
+          <th>Prescribed Product</th>
+          <th>NHS List price/reimbursement cost<sup>1</sup></th>
+          <th>Macilax cost saving vs. alternative Prescribed Product</th>
+          <th>Marketed by</th>
+          </tr>
+</thead>
+        <tbody>
+
+
+          <tr>
+
+            <td>Durogesic® DTrans® (fentanyl) 12 mcg/hr patch 5s </td>
+            <td>£12.59 </td>
+            <td>£5.07 </td>
+            <td>Janssen-Cilag</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Durogesic DTrans (fentanyl) 25 mcg/hr patch 5s </td>
+            <td>£17.99 </td>
+            <td>£7.23 </td>
+            <td>Janssen-Cilag</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Durogesic DTrans (fentanyl) 50 mcg/hr patch 5s</td>
+            <td>£33.66</td>
+            <td>£13.54</td>
+            <td>Janssen-Cilag</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Durogesic DTrans (fentanyl) 75 mcg/hr patch 5s </td>
+            <td>£46.99 </td>
+            <td>£18.93 </td>
+            <td>Janssen-Cilag</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Durogesic DTrans (fentanyl) 100 mcg/hr patch 5s</td>
+            <td>£57.86</td>
+            <td>£23.27</td>
+            <td>Janssen-Cilag</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Fencino® (fentanyl) 12 mcg/hr patch 5s</td>
+            <td>£8.46</td>
+            <td>£0.94</td>
+            <td>Dallas Burston Ashbourne</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Fencino (fentanyl) 25 mcg/hr patch 5s</td>
+            <td>£12.10</td>
+            <td>£1.34</td>
+            <td>Dallas Burston Ashbourne</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Fencino (fentanyl) 50 mcg/hr patch 5s</td>
+            <td>£22.62</td>
+            <td>£2.50</td>
+            <td>Dallas Burston Ashbourne</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Fencino (fentanyl) 75 mcg/hr patch 5s</td>
+            <td>£31.54</td>
+            <td>£3.48</td>
+            <td>Dallas Burston Ashbourne</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Fencino (fentanyl) 100 mcg/hr patch 5s</td>
+            <td>£38.88</td>
+            <td>£4.29</td>
+            <td>Dallas Burston Ashbourne</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Fentalis® Reservoir (fentanyl) 25 mcg/hr patch 5s</td>
+            <td>£22.89</td>
+            <td>£12.13</td>
+            <td>Sandoz Ltd</td>
+
+          </tr>
+
+
+          <tr class="last">
+
+            <td>Fentalis Reservoir (fentanyl) 50 mcg/hr patch 5s</td>
+            <td>£42.77</td>
+            <td>£22.65</td>
+            <td>Sandoz Ltd</td>
+
+          </tr>
+
+
+ 
+        </tbody>
+
+        </table>  
+
+
 
 
        <p class="side_note">Date of preparation: July 2014  UK/SPL/14/0017g</p>
