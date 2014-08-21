396cd21072adc2a69dcf5cf580455401606a068b
garrypulsar <garrybain@apshqmac158.apsgroup.local>
2014-08-21 09:02:55 +0100

    more tables for Marol

--- presentations/08_presentation_marol/04_slide_competitor_comparisons_continued/index.php
+++ presentations/08_presentation_marol/04_slide_competitor_comparisons_continued/index.php
@@ -7,8 +7,114 @@
 
 
 
- <img class="fullscreen" src="img/competitor_comparisons.png" alt="" />                               
-    
+ <!-- <img class="fullscreen" src="img/competitor_comparisons.png" alt="" /> -->
+
+   <table>
+
+<thead>
+        <tr>
+          <th>Prescribed Product</th>
+          <th>NHS List price/reimbursement cost<sup>1</sup></th>
+          <th>Marol<sup>®</sup> cost saving vs. alternative Prescribed Product</th>
+          <th>Marketed by</th>
+          </tr>
+</thead>
+        <tbody>
+
+
+          <tr>
+
+            <td>Maxitram® (tramadol) SR 100 mg Caps 60s</td>
+            <td>£12.14</td>
+            <td>£5.16</td>
+            <td>Chiesi Limited</td>
+
+          </tr>
+
+
+          <tr>
+
+            <td>Maxitram (tramadol) SR 150 mg Caps 60s</td>
+            <td>£18.21</td>
+            <td>£7.73</td>
+            <td>Chiesi Limited</td>
+
+          </tr>
+
+
+         <tr>
+
+            <td>Maxitram (tramadol) SR 200 mg Caps 60s</td>
+            <td>£24.28</td>
+            <td>£10.00</td>
+            <td>Chiesi Limited</td>
+
+          </tr>
+
+
+         <tr>
+
+            <td>Tramquel® (tramadol) SR 100 mg Caps 60s</td>
+            <td>£14.47</td>
+            <td>£7.49</td>
+            <td>Meda Pharmaceuticals Limited</td>
+
+          </tr>
+
+
+         <tr>
+
+            <td>Tramquel (tramadol) SR 150 mg Caps 60s</td>
+            <td>£21.71</td>
+            <td>£11.23</td>
+            <td>Meda Pharmaceuticals Limited</td>
+
+          </tr>
+
+
+         <tr>
+
+            <td>Tramquel (tramadol) SR 200 mg Caps 60s</td>
+            <td>£28.93</td>
+            <td>£14.65</td>
+            <td>Meda Pharmaceuticals Limited</td>
+
+          </tr>
+
+
+         <tr>
+
+            <td>Tramulief® (tramadol) SR 100 mg Tabs 60s</td>
+            <td>£6.98</td>
+            <td>£0.00</td>
+            <td>Sovereign Medical</td>
+
+          </tr>
+
+
+         <tr>
+
+            <td>Tramulief (tramadol) SR 150 mg Tabs 60s</td>
+            <td>£10.48</td>
+            <td>£0.00</td>
+            <td>Sovereign Medical</td>
+
+          </tr>
+
+
+         <tr class="last">
+
+            <td>Tramulief (tramadol) SR 200 mg Tabs 60s</td>
+            <td>£14.28</td>
+            <td>£0.00</td>
+            <td>Sovereign Medical</td>
+
+          </tr>
+
+
+        </tbody>
+
+        </table>     
 
 
        <p class="side_note">Date of preparation: July 2014  UK/SPL/14/0017g</p>
--- presentations/08_presentation_marol/04_slide_competitor_comparisons_continued/js/script.js
+++ presentations/08_presentation_marol/04_slide_competitor_comparisons_continued/js/script.js
@@ -1,6 +1,6 @@
 
 var duration = 800;
 
-$('#slide_container img').delay((duration - 100) * 1).animate({opacity:1}, duration);
+$('#slide_container table').delay((duration - 100) * 1).animate({opacity:1}, duration);
 $('.side_note').delay((duration - 100) * 2).animate({opacity:1}, duration);
 
