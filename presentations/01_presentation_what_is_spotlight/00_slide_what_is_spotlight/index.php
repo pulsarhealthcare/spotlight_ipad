40a60a0be39d5a841cc201eaf88c742ff01343a4
garrypulsar <garrybain@apshqmac158.apsgroup.local>
2014-08-28 09:53:56 +0100

    Missing PDF images

--- presentations/01_presentation_what_is_spotlight/00_slide_what_is_spotlight/index.php
+++ presentations/01_presentation_what_is_spotlight/00_slide_what_is_spotlight/index.php
@@ -5,17 +5,15 @@
 <section style="background:url(img/background.png);background-size:100% 100%;" id="slide_container">
             <h1>Introduction to Spotlight...</h1>
 
-            <h2>Spotlight is the name of a range of branded <br>
-            medicines available exclusively from Teva UK<br>
-            Limited, the largest supplier of medicines to</h2>
-            <h2> the NHS by packs<sup>1,2</sup></h2>
+            <h2>Spotlight is the name of a range of branded
+            medicines available exclusively from Teva UK
+            Limited, the largest supplier of medicines to <span>the NHS by packs<sup>1,2</sup></span></h2>
+
+            <img src="img/graph.gif" alt="" class="fullscreen" />
+
     
             <p class="side_note">Date of preparation: July 2014 UK/SPL/14/0017</p>
-            <div class="chart_container fullscreen">
-            <h3>Annual volume (to August 2013) of packs supplied to the NHS<sup>1,2</sup></h3>
-            <h4 >VAT Volume (masjdjdh)</h4>
-            <div class="chart"></div>
-            </div>
+
 </section>
         
 <?php require $root.'/global/html/footer.html';?>
