7ce9af882393805e8d827520f9c9dc9378ee1929
unknown <C_FXA@APSHQW652.APSGROUP.LOCAL>
2014-09-02 14:35:58 +0100

    test

--- presentations/01_presentation_what_is_spotlight/07_slide_reimbursement_england_and_wales/js/script.js
+++ presentations/01_presentation_what_is_spotlight/07_slide_reimbursement_england_and_wales/js/script.js
@@ -4,9 +4,10 @@
 //Fade text
 var duration = 800;
 
-$('#slide_container #main').delay((duration - 100) * 1).animate({opacity:1}, duration);
-$('#slide_container #main_1').delay((duration - 100) * 2).animate({opacity:1}, duration);
-$('#slide_container #main_2').delay((duration - 100) * 3).animate({opacity:1}, duration);
+
+$('.table_header').delay((duration - 100) * 1).animate({opacity:1}, duration);
+$('.table_colum').eq(0).delay((duration - 100) * 2).animate({opacity:1}, duration);
+$('.table_colum').eq(1).delay((duration - 100) * 3).animate({opacity:1}, duration);
 $('#notes p').delay((duration - 100) * 4).animate({opacity:1}, duration);
 $('.side_note').delay((duration - 100) * 5).animate({opacity:1}, duration);
 
